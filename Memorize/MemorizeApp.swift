ebdc62b539431a7be23886fe0a477e38e9dcac31
Fredrik Öller <fredrik.oller@vwfs.se>
2022-01-08 18:26:10 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Fredrik Öller on 2022-01-08.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
